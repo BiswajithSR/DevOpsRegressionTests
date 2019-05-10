3ad3b819ea9c131556daa95dd35cc47e9b2e88d7
BISWAJITH, SR <biswajithsr@hotmail.com>
2019-05-10 17:23:06 +0530

    maven project added

---
+++ com.test.server/src/main/java/regressionTests/App.java
@@ -0,0 +1,13 @@
+package regressionTests;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
